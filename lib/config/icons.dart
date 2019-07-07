2bcc934d226090901c7bc7e3c7cee9153d849191
artemmmon <thumanazar@yandex.ru>
2019-07-07 14:32:12 +0300

    - app published

--- lib/config/icons.dart
+++ lib/config/icons.dart
@@ -52,7 +52,7 @@ import 'package:flutter/widgets.dart';
 ///         License:   SIL ()
 ///         Homepage:  http://fortawesome.github.com/Font-Awesome/
 ///
-
+/// <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
 class CustomIcons {
   CustomIcons._();
